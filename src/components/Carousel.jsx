63c99b1ab01e36bc53feeb680ad20072a98a2bb2
osmar <osmarernesto17@gmail.com>
2023-09-25 17:43:02 -0600

    fix: carousel didn't working in mobile

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,29 +6,30 @@ import carousel4 from '../img/carousel4.jpg';
 function Carousel() {
     return (
  
-            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    < button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-
-                </div>
-                <div className="carousel-inner ">
-                    <div className="carousel-item active " data-bs-interval="2000">
-                        <img src={carousel1} className="d-block w-100" alt="..."  />
-                    </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <img src={carousel2} className="d-block w-100 " alt="..." />
-                    </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <img src={carousel3} className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <img src={carousel4} className="d-block w-100" alt="..." />
-                    </div>
-                </div>
-            </div>
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={carousel1} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={carousel2} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={carousel3} class="d-block w-100" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img src={carousel4} class="d-block w-100" alt="..."/>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
             );
 }
 export default Carousel;
