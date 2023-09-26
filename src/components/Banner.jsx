210e9485695758366582833acfdaa58c82fc56ba
osmar <osmarernesto17@gmail.com>
2023-09-25 20:42:08 -0600

    feat: added Banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Banner = (props) => {
+    return (
+        <div className="p-5 text-center bg-image rounded-3" style={{
+            backgroundImage: `url(${props.img})`,
+            height: "700px",
+            position: "relative", 
+        }}>
+            <div className="mask"
+                 style={{
+                     backgroundColor: "rgba(0, 0, 0, 0.6)",
+                     position: "absolute", 
+                     top: 0, 
+                     left: 0, 
+                     width: "100%", 
+                     height: "100%" 
+                 }}>
+                <div className="d-flex justify-content-center align-items-center h-100">
+                    <div className="text-white">
+                        <h1 className="mb-3">{props.text}</h1>
+                        <h5 className='mb-3'>{props.secondary}</h5>
+                        <a className="btn btn-outline-light btn-lg" href="#!" role="button">{props.textButton}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import Banner from "../Banner";
+import Reparacion from "../../img/reparacion.jpg"
+import DownloadMode from "../../img/downloadmode.jpg"
 function Services() {
     return (
         <div>
-            <h1>pagina de inicio</h1>
+            <Banner img={Reparacion} text="Reparamos todos los modelos de telefonos" secondary="Cambio de pantalla - cambio de bateria - radiofrecuencia - microsoldadura - puerto de carga" textButton="Contactar"/>
+            <Banner img={DownloadMode} text="Software a todos los modelos de telefonos" secondary="Flasheo - FRP - Bypass cuenta mi - Desbloqueo de red - Reparacion de certificados" textButton="Contactar"/>
         </div>
     );
 }
